a6d0504c6670e4293d3650d09df7e5f996732938
kawzar <hmacarena@gmail.com>
2021-01-09 21:42:11 -0300

    counting stuff

---
+++ server/analytics.js
@@ -0,0 +1,25 @@
+const mongodb = require("mongodb");
+const moment = require("moment");
+
+async function getTotalVisits() {
+  const analytics = await loadAnalyticsCollection();
+  return await analytics.estimatedDocumentCount({ ip: { $nin: [null, ""] } });
+}
+
+async function logVisitor(ip) {
+  const analytics = await loadAnalyticsCollection();
+  await analytics.insertOne({ ip: ip, date: moment().toDate() });
+}
+
+async function loadAnalyticsCollection() {
+  const client = await mongodb.MongoClient.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  return client.db("gallery").collection("analytics");
+}
+
+module.exports = {
+  logVisitor,
+  getTotalVisits,
+};
